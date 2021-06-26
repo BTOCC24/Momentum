05ea195a03f94952db28ad5ca369fb456c829b9a
BTOCC24 <kisses6938@gmail.com>
2021-06-26 17:37:49 +0900

    quote

---
+++ quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+    {
+        quote: "i'm hungry",
+        author: "park",
+    },
+    {
+        quote: "i'm football master",
+        author: "dong",
+    },
+    {
+        quote: "i'm ezreal",
+        author: "hun",
+    },
+]
